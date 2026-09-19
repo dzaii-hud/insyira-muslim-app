2e1c18b61e25211b6a714d62363ed1a3de33cfcc
dzaii-hud <zafahudzaifah@gmail.com>
2026-09-19 12:21:46 +0700

    chore(mobile): ikon aplikasi, konfigurasi Android/iOS, dan aset audio dzikir
    - ikon launcher Android & iOS hasil flutter_launcher_icons
    
    - adaptive icon, colors.xml, dan penyesuaian gradle
    
    - tambah aset audio dzikir (19 MB) dan gambar (bg_login, logo_insyira)
    
    - .gitignore: abaikan android/build/ dan test-file/
    
    Catatan: android/app/google-services.json sengaja TIDAK di-commit (berisi konfigurasi Firebase).

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,10 @@ import audioplayers_darwin
 import flutter_local_notifications
 import geocoding_darwin
 import geolocator_apple
+import google_sign_in_ios
 import package_info_plus
 import shared_preferences_foundation
+import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,7 +20,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeocodingDarwinPlugin.register(with: registry.registrar(forPlugin: "GeocodingDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
